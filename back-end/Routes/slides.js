82e9d0b4240e0699d38dacf3bbc47538d00bf13c
Gabriel-Nog <nogueirajoao799@gmail.com>
2025-03-27 19:34:34 -0300

    Remove unused files and configurations from front-end and back-end

--- back-end/Routes/slides.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { showAll } = require('../controllers/Slide'); // Import slides controller
-
-// Route to get all slides
-router.get('/', showAll);
-
-module.exports = router;
